d1c043008044d5f66a79729b2247030186d7e31b
davorinsavovic <davorinsavovic@gmail.com>
2026-04-24 22:15:37 -0700

    Clover

--- services/payment-service-factory.js
+++ services/payment-service-factory.js
@@ -169,7 +169,7 @@ class PaymentServiceFactory {
     const axios = require('axios');
     const ecomBase =
       config.cloverConfig.environment === 'production'
-        ? 'https://scl.clover.com'
+        ? 'https://api.clover.com'
         : 'https://scl-sandbox.dev.clover.com';
 
     console.log('✅ Using Clover Ecommerce API at:', ecomBase);
