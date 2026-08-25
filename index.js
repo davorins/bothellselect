3e902b6915c11aa760ff49fda6c1e163ed0207f8
davorinsavovic <davorinsavovic@gmail.com>
2026-08-24 22:21:30 -0700

    Update

--- index.js
+++ index.js
@@ -110,7 +110,8 @@ app.use(
   squareWebhooksRouter,
 );
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Use routes
 app.use('/api/ads', advertisementRoutes);
